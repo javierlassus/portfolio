ba47f8f09958f63f47fab6f2b089aae49ccf53a0
Javier Lassus <77644743+javierlassus@users.noreply.github.com>
2021-05-25 18:27:17 -0400

    Add Active class for the NavBar

--- src/components/pages/MobileNav.jsx
+++ src/components/pages/MobileNav.jsx
@@ -1,29 +1,29 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 export default function MobileNav() {
   return (
     <nav className="nav">
-      <Link className="nav--link" to="/">
+      <NavLink exact activeClassName="active" className="nav--link" to="/">
         <div className="nav--link__cont">
           <p className="nav--link__cont__text">HOME</p>
         </div>
-      </Link>
-      <Link className="nav--link" to="/projects">
+      </NavLink>
+      <NavLink activeClassName="active" className="nav--link" to="/projects">
         <div className="nav--link__cont">
           <p className="nav--link__cont__text">PROJECTS</p>
         </div>
-      </Link>
-      <Link className="nav--link" to="/contact">
+      </NavLink>
+      <NavLink activeClassName="active" className="nav--link" to="/contact">
         <div className="nav--link__cont">
           <p className="nav--link__cont__text">CONTACT</p>
         </div>
-      </Link>
-      <Link className="nav--link" to="/resume">
+      </NavLink>
+      <NavLink activeClassName="active" className="nav--link" to="/resume">
         <div className="nav--link__cont">
           <p className="nav--link__cont__text">RESUME</p>
         </div>
-      </Link>
+      </NavLink>
     </nav>
   );
 }
