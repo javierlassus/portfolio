d2c3bc2c6b84df3e505ddba211820feb0243c0d6
Javier Lassus <77644743+javierlassus@users.noreply.github.com>
2021-05-25 14:57:15 -0400

    Add Header and style to it

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,10 +3,8 @@ import "./Components.scss";
 
 export default function Header() {
   return (
-    <header>
-      <div>
-        <h2>JAVIER LASSUS</h2>
-      </div>
+    <header className="header">
+      <h2 className="header--title">JAVIER LASSUS</h2>
     </header>
   );
 }
