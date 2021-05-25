3d34f6512fb972f1f6df0de36b2fb13185a2dcff
Javier Lassus <77644743+javierlassus@users.noreply.github.com>
2021-05-25 13:14:23 -0400

    Added Routes and Components and pages for it

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
-function App() {
-  return <div className="App">Hello</div>;
-}
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Redirect,
+  Switch,
+  Route,
+} from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./components/pages/Home";
+import Projects from "./components/pages/Projects";
+import Contact from "./components/pages/Contact";
+import Resume from "./components/pages/Resume";
 
-export default App;
+export default function App() {
+  return (
+    <Router>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/projects" component={Projects} />
+        <Route path="/contact" component={Contact} />
+        <Route path="/resume" component={Resume} />
+      </Switch>
+    </Router>
+  );
+}
