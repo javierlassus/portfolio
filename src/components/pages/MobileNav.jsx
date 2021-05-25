712f984a930e7904df04bb1878775f5b0e969cd1
Javier Lassus <77644743+javierlassus@users.noreply.github.com>
2021-05-25 15:49:20 -0400

    Mobile NavBar basic style done

--- src/components/pages/MobileNav.jsx
+++ src/components/pages/MobileNav.jsx
@@ -3,31 +3,27 @@ import { Link } from "react-router-dom";
 
 export default function MobileNav() {
   return (
-    <div>
-      <nav>
-        <ul>
-          <Link to="/">
-            <div>
-              <p>HOME</p>
-            </div>
-          </Link>
-          <Link to="/projects">
-            <div>
-              <p>PROJECTS</p>
-            </div>
-          </Link>
-          <Link to="/contact">
-            <div>
-              <p>CONTACT</p>
-            </div>
-          </Link>
-          <Link to="/resume">
-            <div>
-              <p>RESUME</p>
-            </div>
-          </Link>
-        </ul>
-      </nav>
-    </div>
+    <nav className="nav">
+      <Link className="nav--link" to="/">
+        <div className="nav--link__cont">
+          <p className="nav--link__cont__text">HOME</p>
+        </div>
+      </Link>
+      <Link className="nav--link" to="/">
+        <div className="nav--link__cont">
+          <p className="nav--link__cont__text">PROJECTS</p>
+        </div>
+      </Link>
+      <Link className="nav--link" to="/">
+        <div className="nav--link__cont">
+          <p className="nav--link__cont__text">CONTACT</p>
+        </div>
+      </Link>
+      <Link className="nav--link" to="/">
+        <div className="nav--link__cont">
+          <p className="nav--link__cont__text">RESUME</p>
+        </div>
+      </Link>
+    </nav>
   );
 }
