28193e0c5b5083468098c25c0b100b3dc829e878
Javier Lassus <77644743+javierlassus@users.noreply.github.com>
2021-06-03 12:25:00 -0400

    Added Social Icons and style them

--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -5,13 +5,13 @@ import Git from "../assets/icons/github.svg";
 
 export default function Icons() {
   return (
-    <div className="cont">
-      {/* <a href="https://www.linkedin.com/in/javierlassus/">
-        <img className="cont--img" src={In} alt="Linked In" />
+    <div className="social">
+      <a href="https://www.linkedin.com/in/javierlassus/">
+        <img className="social--img" src={In} alt="Linked In" />
       </a>
       <a href="https://github.com/javierlassus">
-        <img className="cont--img" src={Git} alt="GitHub" />
-      </a> */}
+        <img className="social--img" src={Git} alt="GitHub" />
+      </a>
     </div>
   );
 }
