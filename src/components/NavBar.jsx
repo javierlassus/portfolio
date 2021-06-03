74c60445491fb80abfb07600ec0fa0ce9f4ab64f
Javier Lassus <77644743+javierlassus@users.noreply.github.com>
2021-06-03 11:35:01 -0400

    Added Hamburger menu

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,25 +1,27 @@
 import React, { useState } from "react";
 import "./Components.scss";
+import { Squash as Hamburger } from "hamburger-react";
 
 export default function NavBar() {
-  const [open, setOpen] = useState(false);
-
-  const handleToggle = () => {
-    setOpen(!open);
-  };
+  const [isOpen, setOpen] = useState(false);
 
   return (
-    <div className="nav">
+    <nav className="nav">
       <div className="nav--header">
         <h1 className="nav--header__name">Javier Lassus</h1>
         <p className="nav--header--title">Full-Stack Dev</p>
       </div>
       <div className="nav--btn">
         <p className="nav--btn__menu">menu</p>
-        <button className={open ? "nav-open" : null}>
-          <span className="nav--btn__ctn__hamburger"></span>
-        </button>
+        <Hamburger
+          toggled={isOpen}
+          toggle={setOpen}
+          size={25}
+          rounded
+          distance="sm"
+          hideOutline={false}
+        />
       </div>
-    </div>
+    </nav>
   );
 }
