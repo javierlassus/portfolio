7ffab7657e7067f0984f274bfb685d9f090eb385
Javier Lassus <77644743+javierlassus@users.noreply.github.com>
2021-06-06 01:27:22 -0400

    Added function to control when to actiate the scroll button

--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Components.scss";
 import { animateScroll as scroll } from "react-scroll";
 import In from "../assets/icons/linkedin.svg";
@@ -6,20 +6,39 @@ import Git from "../assets/icons/github.svg";
 import Up from "../assets/icons/up-arrow.svg";
 
 export default function Icons() {
+  //TO TARGET THE SCROLL AND ACTIVATE THE BUTTON TO SCROLL UP
+  const [visible, setVisible] = useState(false);
+
+  const toggleVisibility = () => {
+    if (window.pageYOffset > 300) {
+      setVisible(true);
+    } else {
+      setVisible(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", toggleVisibility);
+  }, []);
+
+  //TO SCROLL UP ON CLICK
   const scrollToTop = () => {
     scroll.scrollToTop();
   };
 
   return (
     <div className="social">
-      <div className="social--scroll">
-        <img
-          onClick={scrollToTop}
-          className="social--scroll__img"
-          src={Up}
-          alt="Scroll Up"
-        />
-      </div>
+      {visible && (
+        <div className="social--scroll">
+          <img
+            onClick={scrollToTop}
+            className="social--scroll__img"
+            src={Up}
+            alt="Scroll Up"
+          />
+        </div>
+      )}
+
       <a href="https://www.linkedin.com/in/javierlassus/">
         <img className="social--img" src={In} alt="Linked In" />
       </a>
