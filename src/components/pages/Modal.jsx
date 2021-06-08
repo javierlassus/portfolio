99f3ab104d7717e27b6ef7faa00cd48d91569d0b
Javier Lassus <77644743+javierlassus@users.noreply.github.com>
2021-06-08 13:26:37 -0400

    Added style and functionality to modal to work with the hamburger menu

--- src/components/pages/Modal.jsx
+++ src/components/pages/Modal.jsx
@@ -1,20 +1,27 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { Link as Scroll } from "react-scroll";
 import "../Components.scss";
 
-export default function Modal({ modal, setModal }) {
-  //TO CLOSE MODAL BY THE X
-  //   const handleClick = (e) => {
-  //     if (modal !== e.target) {
-  //       setModal(false);
-  //     }
-  //   };
-
+export default function Modal({ modal }) {
   return (
     <>
       {modal ? (
-        <div className="modal">
-          <p>Hello</p>
-        </div>
+        <nav className="modal">
+          <p className="modal--menu">MENU</p>
+          <Link className="link" activeClassName="active" to="/">
+            <p className="modal--pages">PROJECTS</p>
+          </Link>
+          <Link className="link" activeClassName="active" to="/about">
+            <p className="modal--pages">ABOUT ME</p>
+          </Link>
+          <Link className="link" activeClassName="active" to="/contact">
+            <p className="modal--pages">CONTACT ME</p>
+          </Link>
+          <Link className="link" activeClassName="active" to="/resume">
+            <p className="modal--pages">RESUME</p>
+          </Link>
+        </nav>
       ) : null}
     </>
   );
