e4aa387fbbc480d83d0d0bc6a19fb0737fcff0f7
Javier Lassus <77644743+javierlassus@users.noreply.github.com>
2021-06-09 15:05:15 -0400

    Changed Warnings so no issues on ddeployment

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import "./Components.scss";
 import { Squash as Hamburger } from "hamburger-react";
